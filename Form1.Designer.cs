9ef78b01304459e4abae3e7a7536da073cd22551
Admin <Admin@ZIYA>
2022-08-13 14:56:06 +0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Lesso3._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listform1 = new System.Windows.Forms.ListBox();
+            this.addproduct = new System.Windows.Forms.Button();
+            this.editproduct = new System.Windows.Forms.Button();
+            this.removeproduct = new System.Windows.Forms.Button();
+            this.clearlist = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listform1
+            // 
+            this.listform1.FormattingEnabled = true;
+            this.listform1.ItemHeight = 15;
+            this.listform1.Location = new System.Drawing.Point(29, 25);
+            this.listform1.Name = "listform1";
+            this.listform1.Size = new System.Drawing.Size(220, 259);
+            this.listform1.TabIndex = 0;
+            // 
+            // addproduct
+            // 
+            this.addproduct.Location = new System.Drawing.Point(29, 307);
+            this.addproduct.Name = "addproduct";
+            this.addproduct.Size = new System.Drawing.Size(220, 31);
+            this.addproduct.TabIndex = 1;
+            this.addproduct.Text = "Add Product";
+            this.addproduct.UseVisualStyleBackColor = true;
+            // 
+            // editproduct
+            // 
+            this.editproduct.Location = new System.Drawing.Point(29, 344);
+            this.editproduct.Name = "editproduct";
+            this.editproduct.Size = new System.Drawing.Size(220, 31);
+            this.editproduct.TabIndex = 2;
+            this.editproduct.Text = "Edit Product";
+            this.editproduct.UseVisualStyleBackColor = true;
+            // 
+            // removeproduct
+            // 
+            this.removeproduct.Location = new System.Drawing.Point(29, 381);
+            this.removeproduct.Name = "removeproduct";
+            this.removeproduct.Size = new System.Drawing.Size(220, 31);
+            this.removeproduct.TabIndex = 3;
+            this.removeproduct.Text = "Remove Product";
+            this.removeproduct.UseVisualStyleBackColor = true;
+            // 
+            // clearlist
+            // 
+            this.clearlist.Location = new System.Drawing.Point(29, 418);
+            this.clearlist.Name = "clearlist";
+            this.clearlist.Size = new System.Drawing.Size(220, 31);
+            this.clearlist.TabIndex = 4;
+            this.clearlist.Text = "Clear List";
+            this.clearlist.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 466);
+            this.Controls.Add(this.clearlist);
+            this.Controls.Add(this.removeproduct);
+            this.Controls.Add(this.editproduct);
+            this.Controls.Add(this.addproduct);
+            this.Controls.Add(this.listform1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ListBox listform1;
+        private Button addproduct;
+        private Button editproduct;
+        private Button removeproduct;
+        private Button clearlist;
+    }
+}
---
+++ Form2.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Lesso3._1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.productname = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.origincountry = new System.Windows.Forms.Label();
+            this.productcost = new System.Windows.Forms.Label();
+            this.okbtn = new System.Windows.Forms.Button();
+            this.canselbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // productname
+            // 
+            this.productname.AutoSize = true;
+            this.productname.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.productname.Location = new System.Drawing.Point(47, 66);
+            this.productname.Name = "productname";
+            this.productname.Size = new System.Drawing.Size(87, 15);
+            this.productname.TabIndex = 0;
+            this.productname.Text = "Product Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 84);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(47, 144);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(169, 29);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(47, 215);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(169, 29);
+            this.textBox3.TabIndex = 3;
+            // 
+            // origincountry
+            // 
+            this.origincountry.AutoSize = true;
+            this.origincountry.Location = new System.Drawing.Point(47, 126);
+            this.origincountry.Name = "origincountry";
+            this.origincountry.Size = new System.Drawing.Size(89, 15);
+            this.origincountry.TabIndex = 4;
+            this.origincountry.Text = "Origin Country:";
+            // 
+            // productcost
+            // 
+            this.productcost.AutoSize = true;
+            this.productcost.Location = new System.Drawing.Point(47, 187);
+            this.productcost.Name = "productcost";
+            this.productcost.Size = new System.Drawing.Size(79, 15);
+            this.productcost.TabIndex = 5;
+            this.productcost.Text = "Product Cost:";
+            // 
+            // okbtn
+            // 
+            this.okbtn.Location = new System.Drawing.Point(47, 275);
+            this.okbtn.Name = "okbtn";
+            this.okbtn.Size = new System.Drawing.Size(62, 32);
+            this.okbtn.TabIndex = 6;
+            this.okbtn.Text = "Ok";
+            this.okbtn.UseVisualStyleBackColor = true;
+            // 
+            // canselbtn
+            // 
+            this.canselbtn.Location = new System.Drawing.Point(154, 275);
+            this.canselbtn.Name = "canselbtn";
+            this.canselbtn.Size = new System.Drawing.Size(62, 32);
+            this.canselbtn.TabIndex = 7;
+            this.canselbtn.Text = "Cancel";
+            this.canselbtn.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 362);
+            this.Controls.Add(this.canselbtn);
+            this.Controls.Add(this.okbtn);
+            this.Controls.Add(this.productcost);
+            this.Controls.Add(this.origincountry);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.productname);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label productname;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label origincountry;
+        private Label productcost;
+        private Button okbtn;
+        private Button canselbtn;
+    }
+}
